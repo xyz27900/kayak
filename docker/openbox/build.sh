f44fc31fb1147e9717e44c62bf2e7e09c8b30ac0
Evgeniy Kozlov <kozlov.work.52@gmail.com>
2023-01-13 11:09:34 +0200

    Test Docker

--- docker/openbox/build.sh
+++ docker/openbox/build.sh
@@ -35,15 +35,8 @@ function log {
 # we have very few fonts installed.
 #
 
-#log "Installing fontconfig..."
-#cp -av /tmp/fontconfig-install/usr $(xx-info sysroot)
-
-#
-# Build Openbox.
-#
-#mkdir /tmp/openbox
-#log "Downloading Openbox..."
-#curl -# -L ${OPENBOX_URL} | tar -xJ --strip 1 -C /tmp/openbox
+log "Installing fontconfig..."
+cp -av /tmp/fontconfig-install/usr $(xx-info sysroot)
 
 log "Patching Openbox..."
 patch -p1 -d /tmp/openbox < "$SCRIPT_DIR"/disable-x-locale.patch
@@ -51,28 +44,26 @@ patch -p1 -d /tmp/openbox < "$SCRIPT_DIR"/menu-file-order.patch
 
 # The config.sub provided with Openbox is too old.  Get a recent one from
 # https://github.com/gcc-mirror/gcc/blob/master/config.sub
-cp -v "$SCRIPT_DIR"/config.sub /tmp/openbox
+#cp -v "$SCRIPT_DIR"/config.sub /tmp/openbox
 
 log "Configuring Openbox..."
 (
-    #cd /tmp/openbox && LIBS="$LDFLAGS" ./configure \
-
-    cd /tmp/openbox && \
-        OB_LIBS="-lX11 -lxcb -lXdmcp -lXau -lXext -lXft -lXrandr -lfontconfig -lfreetype -lpng -lXrender -lexpat -lxml2 -lz -lbz2 -llzma -lbrotlidec -lbrotlicommon -lintl -lfribidi -lharfbuzz -lpangoxft-1.0 -lpangoft2-1.0 -lpango-1.0 -lgio-2.0 -lgobject-2.0 -lglib-2.0 -lpcre -lgraphite2 -lffi" \
-        LDFLAGS="$LDFLAGS -Wl,--start-group $OB_LIBS -Wl,--end-group" LIBS="$LDFLAGS" ./configure \
-        --build=$(TARGETPLATFORM= xx-clang --print-target-triple) \
-        --host=$(xx-clang --print-target-triple) \
-        --prefix=/usr \
-        --datarootdir=/opt/base/share \
-        --disable-shared \
-        --enable-static \
-        --disable-nls \
-        --disable-startup-notification \
-        --disable-xcursor \
-        --disable-librsvg \
-        --disable-session-management \
-        --disable-xkb \
-        --disable-xinerama \
+cd /tmp/openbox && \
+    OB_LIBS="-lX11 -lxcb -lXdmcp -lXau -lXext -lXft -lXrandr -lfontconfig -lfreetype -lpng -lXrender -lexpat -lxml2 -lz -lbz2 -llzma -lbrotlidec -lbrotlicommon -lintl -lfribidi -lharfbuzz -lpangoxft-1.0 -lpangoft2-1.0 -lpango-1.0 -lgio-2.0 -lgobject-2.0 -lglib-2.0 -lpcre -lgraphite2 -lffi" \
+    LDFLAGS="$LDFLAGS -Wl,--start-group $OB_LIBS -Wl,--end-group" LIBS="$LDFLAGS" ./configure \
+    --build=$(TARGETPLATFORM= xx-clang --print-target-triple) \
+    --host=$(xx-clang --print-target-triple) \
+    --prefix=/usr \
+    --datarootdir=/opt/base/share \
+    --disable-shared \
+    --enable-static \
+    --disable-nls \
+    --disable-startup-notification \
+    --disable-xcursor \
+    --disable-librsvg \
+    --disable-session-management \
+    --disable-xkb \
+    --disable-xinerama \
 )
 
 log "Compiling Openbox..."
